c0270ef9ec8bc40bf7a8c5e9c421a2b0b35a4b6f
dofuerer <domenicluca.fuerer@uzh.ch>
2022-04-14 15:07:56 +0200

    exchanged address

--- src/consts/addresses.const.js
+++ src/consts/addresses.const.js
@@ -1,5 +1,5 @@
 const addressesConst = {
-  bettingAddress: '0x5628f4a9E17f7afF58B6Dd78dc438869a5d9a774',
+  bettingAddress: '0x43c23e39d14E1154D381aBb663bD1E0f0fF88cd2',
   emptyAddress: '0x0000000000000000000000000000000000000000',
 };
 
