520696520fbd5064a782e30792071f2fa0f0dd5e
dofuerer <domenicluca.fuerer@uzh.ch>
2022-04-29 13:50:33 +0200

    changed waiting time for reset game

--- src/consts/countdown.const.js
+++ src/consts/countdown.const.js
@@ -1,6 +1,6 @@
 const countDownTimings = {
-  countDownResetTime: 90 * 1000, // 2 minute, in milliseconds
-  timeUntilRaceStarts: 3 * 60, // 2 minutes, in seconds
+  countDownResetTime: 90 * 1000, // 1.5 minute, in milliseconds
+  timeUntilRaceStarts: 3 * 60, // 3 minutes, in seconds
   waitTimeForNonAdmins: 10 * 1000, // 10 seconds, in milliseconds
   betsProhibitTiming: 60 // in seconds
 };
