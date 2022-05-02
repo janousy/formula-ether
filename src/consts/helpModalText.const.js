2e521f5486dfedadfc000304eac05f6d369fbbc9
dofuerer <domenicluca.fuerer@uzh.ch>
2022-05-02 14:14:25 +0200

    update in help text

--- src/consts/helpModalText.const.js
+++ src/consts/helpModalText.const.js
@@ -4,4 +4,4 @@ export const helpModalText = 'Welcome to Formula Ether\n' +
     '- You can only bet on one team per race. Betting is only possible until the race counter turns red, which is 1minute before start.\n' +
     '- There is a maximum of 20players that can participate.\n' +
     '- Wait for the race to start.\n\n' +
-    'If you are selected as admin. Please confirm the race start';
+    'If you are selected as race director. Please confirm the race start';
