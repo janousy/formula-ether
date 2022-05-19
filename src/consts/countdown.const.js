38bf1df544a7d5f4fad42f6e20c4e2222b72e15f
janosch <janosch.baltensperger>
2022-05-19 13:47:56 +0200

    resolve bug on tx failure.

--- src/consts/countdown.const.js
+++ src/consts/countdown.const.js
@@ -1,8 +1,8 @@
 const countDownTimings = {
   countDownResetTime: 60 * 1000, // 1 minute, in milliseconds
-  timeUntilRaceStarts: 2 * 60, // 2 minutes, in seconds
+  timeUntilRaceStarts: 3 * 60, // 2 minutes, in seconds
   waitTimeForNonAdmins: 10 * 1000, // 10 seconds, in milliseconds
-  betsProhibitTiming: 30 // in seconds
+  betsProhibitTiming: 15 // in seconds
 };
 
 export default countDownTimings
--- src/consts/pollingTimes.const.js
+++ src/consts/pollingTimes.const.js
@@ -1,4 +1,4 @@
 export const pollingTimes = {
-    totalAmounts: 0.05 * 60 * 1000, // 5 seconds
-    winners: 0.05 * 60 * 1000, // 5 seconds
+    totalAmounts: 0.01 * 60 * 1000, // 5 seconds
+    winners: 0.01 * 60 * 1000, // 5 seconds
 };
